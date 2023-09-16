9f0c1d3162073659fa2700d03d8f65a2df0b3070
tonmoytt <tafsirultonmoy666@gmail.com>
2023-09-16 21:50:17 +0600

    add readme question and answer

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,18 @@ function App() {
 
   const handelerselecetd = selected => {
     const isExested = bookmark.find((item) => item.id === selected.id);
+     
+    
+
+    
     let count =selected.reading_time;
     let Dollar =selected.price;
-
+ 
+  
     if (isExested) {
       return Swal.fire(
-        ' Sorry, this file',
-        ' already added'
+        ' Sorry, This Card',
+       " ' already added'"
       )
     }
     else {
@@ -29,15 +34,17 @@ function App() {
       bookmark.forEach((item) => {
         count = count + item.reading_time;
       });
+      setaddhours(count);
 
       bookmark.forEach((item) => {
         Dollar = Dollar + item.price;
       });
       settotaldollar(Dollar);
       
-      setaddhours(count);
+    
       const removehours= 20-count;
       setremining(removehours);
+
 
       const bookmarkbtn = [...bookmark, selected];
       setbookmark(bookmarkbtn);
@@ -51,17 +58,17 @@ function App() {
       <h1 className='text-center text-4xl text-bold'>Course Registration</h1>
       <div className='grid lg:flex   gap-4'>
         <Course handelerselecetd={handelerselecetd}></Course>
-        <div className='mt-12'>
+        <div className='mt-6'>
           <div>
-            <h2 className="text-xl font-red mb-2 ">Credit Hours Remaining : {remining}</h2>
+            
           </div>
 
-          <Bookmark bookmark={bookmark}> </Bookmark>
+          <Bookmark bookmark={bookmark}   remining={remining}> </Bookmark>
           <div>
           <h2 className="text-2xl mb-3 mt-6">Total credit Hours :{addhours}</h2>
           </div>
           <div className='text-2xl mt-6 '>
-            <h2 className='mb-4'>Total cost :{totaldollar}</h2>
+            <h2 className='mb-4'>Total Price :  {totaldollar} USD</h2>
             <hr />
           </div>
         
--- src/component/Bookmark/Bookmark.jsx
+++ src/component/Bookmark/Bookmark.jsx
@@ -1,14 +1,15 @@
 
 import PropTypes from 'prop-types'
 
-const Bookmark = ({bookmark}) => {
+const Bookmark = ({bookmark,remining}) => {
     // console.log(bookmark);
      
     return (
         <div className=" mt-10 rounded-lg shadow-xl p-4">
-           
+          <h2 className="text-xl font-red mb-2 ">Credit Hours Remaining : {remining}</h2> 
            <h2 className="font-bold text-3xl p-2"> Course Name </h2>
        <hr /> 
+       
        <div>
         {bookmark?.map((course, index) => <ol key={index}>{index + 1}. {course.title}</ol>)}
        </div>
@@ -16,6 +17,7 @@ const Bookmark = ({bookmark}) => {
     );
 };
 Bookmark.propTypes={
-    bookmark:PropTypes.array 
+    bookmark:PropTypes.array,
+     
 }
 export default Bookmark;
